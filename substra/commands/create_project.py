6e5abdb016bd9e764ef81ce0ef2a139e0ee718b5
Jérémy Morel <jeremy.morel@owkin.com>
2019-04-05 11:03:26 +0200

    dynamically set abspaths in create_project

--- substra/commands/create_project.py
+++ substra/commands/create_project.py
@@ -1,8 +1,30 @@
+import json
 import os
 import shutil
 from .base import Base
 
 base_assets_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'create_project_assets')
+
+
+def with_absolute_paths(obj, list_of_keys, target_path):
+    def abspath(path): return os.path.abspath(os.path.join(target_path, path))
+
+    for keys in list_of_keys:
+        (k1, k2) = keys
+        if isinstance(obj[k1][k2], list):
+            obj[k1][k2] = [abspath(path) for path in obj[k1][k2]]
+        else:
+            obj[k1][k2] = abspath(obj[k1][k2])
+    return obj
+
+
+def update_paths_in_json(target_path, filename, list_of_keys):
+    file_path = os.path.join(target_path, filename)
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    data = with_absolute_paths(data, list_of_keys, target_path)
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=2)
 
 
 class CreateProject(Base):
@@ -25,4 +47,18 @@ class CreateProject(Base):
         except Exception as e:
             self.handle_exception(e)
         else:
+            if self.options['isic']:
+                # update paths in JSON files
+                update_paths_in_json(os.path.join(target_path, 'objective'), 'objective.json', [
+                    ['objective', 'description'],
+                    ['objective', 'metrics'],
+                    ['data_manager', 'description'],
+                    ['data_manager', 'data_opener'],
+                    ['data_samples', 'paths']
+                ])
+                update_paths_in_json(os.path.join(target_path, 'dataset'), 'dataset.json', [
+                    ['data_manager', 'description'],
+                    ['data_manager', 'data_opener'],
+                    ['data_samples', 'paths']
+                ])
             print(f'New project created in {target_path}')
