ff3fe0be78354b3a14b9204b2ed10f2069501c49
Samuel <samuel.lesuffleur@owkin.com>
2019-04-18 11:03:33 +0200

    update run-local and isic templates with substratools

--- substra/create_project_assets/isic/algo/algo.py
+++ substra/create_project_assets/isic/algo/algo.py
@@ -1,142 +1,64 @@
 """Submission example with sklearn"""
-import os
+# coding: utf8
 import json
-import logging
-import argparse
+
 import numpy as np
 from sklearn.linear_model import SGDClassifier
 
-import opener
-
-# For now, logging will not be reported in Substra for security reasons
-logging.basicConfig(filename='model/log_model.log', level=logging.DEBUG)
+import substratools as tools
 
 
-def _save_json_sklearn(model):
-    """Save estimated params of a sklearn model in a json file.
-    Does not work for sklearn.ensemble estimators"""
-    attr = model.__dict__
-    estimated_params = {key: value.tolist() if isinstance(value, np.ndarray) else value
-                        for key, value in attr.items() if key[-1] == '_' and key != 'loss_function_'}
-    with open('./model/model', 'w') as f:
-        json.dump(estimated_params, f)
+class ISICAlgo(tools.algo.Algo):
 
+    def dump_sklearn_model(self, model):
+        attr = model.__dict__
+        data = {
+            key: value.tolist() if isinstance(value, np.ndarray) else value
+            for key, value in attr.items()
+            if key[-1] == '_' and key != 'loss_function_'
+        }
+        return data
 
-def _load_json_sklearn(inmodels):
-    """ Load estimated params of a trained sklearn model from a json file"""
-    model = SGDClassifier(warm_start=True, loss='log', random_state=42)
-    if inmodels is not None:
-        # Handle only one inmodel
-        with open(os.path.join('./model', inmodels[0]), 'r') as f:
-            attr = json.load(f)
-        for key, value in attr.items():
-            if isinstance(value, list):
-                setattr(model, key, np.array(value))
-            else:
-                setattr(model, key, value)
-    return model
+    def load_sklearn_model(self, data):
+        model = SGDClassifier(warm_start=True, loss='log', random_state=42)
+        if data is not None:
+            for key, value in data.items():
+                if isinstance(value, list):
+                    setattr(model, key, np.array(value))
+                else:
+                    setattr(model, key, value)
+        return model
 
+    def train(self, X, y, models, rank=0):
+        X = X.reshape(X.shape[0], -1)
+        X_train = np.nan_to_num((X - np.mean(X, axis=0)) / np.std(X, axis=0))
 
-def train(inmodels=None, rank=0):
-    # 1. load data using opener.get_X and opener.get_y
-    
-    # extract data
-    logging.info('Getting data')
-    X_train = opener.get_X()
-    y_train = opener.get_y()
-    # standardize data
-    X_train = X_train.reshape(X_train.shape[0], -1)
-    X_train = np.nan_to_num((X_train - np.mean(X_train, axis=0)) / np.std(X_train, axis=0))
+        model = models[0] if models else None
+        clf = self.load_sklearn_model(model)
 
-    # 2. load pre-existing models
-    #    models are stored in the "./model/" folder 
-    #    the inmodels param is the list of all pre-existing model filenames to use during the training
-    clf = _load_json_sklearn(inmodels)
+        clf.fit(X_train, y.argmax(axis=1))
 
-    # 3. train algorithm and produce new model
-    logging.info('Fitting model')
-    clf.fit(X_train, y_train.argmax(axis=1))
+        y_pred = clf.predict_proba(X_train)
 
-    # 4. save new model
-    #    new model must be saved under the "./model/" folder under the filename "model" (i.e. "./model/model")
-    logging.info('Saving fitted model')
-    _save_json_sklearn(clf)
+        return y_pred, self.dump_sklearn_model(clf)
 
-    # 5. save predictions made on train data (opener.get_X) using opener.save_pred
-    y_pred = clf.predict_proba(X_train)
-    opener.save_pred(y_pred)
+    def predict(self, X, y, model):
+        X = X.reshape(X.shape[0], -1)
+        X_test = np.nan_to_num((X - np.mean(X, axis=0)) / np.std(X, axis=0))
 
+        clf = self.load_sklearn_model(model)
 
-def predict(inmodels=None):
-    # 1. load data using opener.get_X and opener.get_y
-    X_test = opener.get_X()
-    X_test = X_test.reshape(X_test.shape[0], -1)
-    X_test = np.nan_to_num((X_test - np.mean(X_test, axis=0)) / np.std(X_test, axis=0))
+        y_pred = clf.predict_proba(X_test)
+        return y_pred
 
-    # 2. load a model
-    #    the model file name is "model" and is in the "./model/" folder (i.e. "./model/model")
-    #    the inmodels param is a list containing a single element: the filename of the model to load
-    clf = _load_json_sklearn(inmodels)
+    def load_model(self, path):
+        with open(path, 'r') as f:
+            return json.load(f)
 
-    # 3. save predictions made on train data (opener.get_X) using opener.save_pred
-    y_pred = clf.predict_proba(X_test)
-    opener.save_pred(y_pred)
+    def save_model(self, model, path):
+        with open(path, 'w') as f:
+            json.dump(model, f)
 
-
-def dry_run(inmodels=None):
-    # 1. load data using opener.fake_X and opener.fake_y
-
-    # extract data
-    logging.info('Getting data')
-    X_train = opener.fake_X()
-    y_train = opener.fake_y()
-    # standardize data
-    X_train = X_train.reshape(X_train.shape[0], -1)
-    X_train = np.nan_to_num((X_train - np.mean(X_train, axis=0)) / np.std(X_train, axis=0))
-
-    # 2. load pre-existing models
-    #    models are stored in the "./model/" folder 
-    #    the inmodels param is the list of all pre-existing model filenames to use during the training
-    clf = _load_json_sklearn(inmodels)
-
-    # 3. train algorithm and produce new model
-    logging.info('Fitting model')
-    clf.fit(X_train, y_train.argmax(axis=1))
-
-    # 4. save new model
-    #    new model must be saved under the "./model/" folder under the filename "model" (i.e. "./model/model")
-    logging.info('Saving fitted model')
-    _save_json_sklearn(clf)
-
-    # 5. save predictions made on train data (generated at step 1) using opener.save_pred
-    y_pred = clf.predict_proba(X_train)
-    opener.save_pred(y_pred)
 
 if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dry-run', action='store_true', default=False,
-                        help='Launch dry run')
-    parser.add_argument('-t', '--train', action='store_true', default=False,
-                        help='Launch train')
-    parser.add_argument('-p', '--predict', action='store_true', default=False,
-                        help='Launch predict')
-    parser.add_argument('-m', '--inmodels', metavar='TraintupleKey', type=str, nargs='*',
-                        help='List of models from traintuplekey')
-    parser.add_argument('-r', '--rank', type=int, default=0,
-                        help='Rank of the fltask, if one')
-    args = vars(parser.parse_args())
-
-    logging.info(args)
-
-    if args['train']:
-        logging.info('Starting train...')
-        train(inmodels=args['inmodels'], rank=args['rank'])
-    elif args['predict']:
-        logging.info('Starting predict...')
-        predict(inmodels=args['inmodels'])
-    elif args['dry_run']:
-        logging.info('Starting dry run...')
-        dry_run(inmodels=args['inmodels'])
-    else:
-        raise ValueError('task not implemented, should be either train, predict or dry-run')
+    tools.algo.execute(ISICAlgo())
--- substra/create_project_assets/isic/dataset/opener.py
+++ substra/create_project_assets/isic/dataset/opener.py
@@ -4,12 +4,13 @@ import csv
 import numpy as np
 from PIL import Image
 
+import substratools as tools
+
 
 PREFIX_X = "IMG_"
 SUFFIX_X = ".jpg"
 PREFIX_Y = "LABEL_"
 SUFFIX_Y = ".csv"
-PRED_FILE = "pred.csv"
 
 # Data Description
 SIZE_X = 450
@@ -19,109 +20,106 @@ CLASSES = 7
 N_FAKE_SAMPLES = 10
 
 
-def _check_existing_files(folder, files):
+def _check_existing_files(files):
     """check if files from a list of files are located in folder"""
-    for fname in files:
-        if not os.path.isfile(os.path.join(folder, fname)):
-            raise FileNotFoundError("non existing file %s in folder %s" %
-                                    (fname, folder))
+    for f in files:
+        if not os.path.isfile(f):
+            raise FileNotFoundError("non existing file %s" % (f))
 
 
-def _get_files(folder):
+def _get_paths(folders):
     """return list of features and label files given a folder location (with
     the same order)"""
     # get list of features files and create associated list of label files
-    X_files = [os.path.join(subfolder, f) for subfolder in os.listdir(folder)
-               for f in os.listdir(os.path.join(folder, subfolder))
+    X_files = [os.path.join(folder, f) for folder in folders
+               for f in os.listdir(os.path.join(folder))
                if '.jpg' in f]
     y_files = [f.replace(PREFIX_X, PREFIX_Y).replace(SUFFIX_X, SUFFIX_Y) for f in X_files]
+
     # check label files exist
     try:
-        _check_existing_files(folder, y_files)
+        _check_existing_files(y_files)
     except FileNotFoundError as e:
         print(str(e))
         y_files = None
     return X_files, y_files
 
 
-def get_X(folder='./data'):
-    """Format and return the ISIC features data as np arrays.
+class ISICOpener(tools.Opener):
 
-    :param folder: path to the folder where all data used by the current training task have been unarchived
-    :type folder: string
-    :return: matrix of features
-    :rtype: numpy array
-    """
-    print('Finding features files...')
-    X_files, _ = _get_files(folder)
-    print('Loading features...')
-    X = []
-    for f in X_files:
-        image = Image.open(os.path.join(folder, f))
-        X.append(np.array(image))
-    return np.array(X)
+    def get_X(self, folders):
+        """Format and return the ISIC features data as np arrays.
 
+        :param folders: path to the data sample folders
+        :type folder: list of strings
+        :return: matrix of features
+        :rtype: numpy array
+        """
+        print('Finding features files...')
+        X_paths, _ = _get_paths(folders)
+        print('Loading features...')
+        X = []
+        for path in X_paths:
+            image = Image.open(path)
+            X.append(np.array(image))
+        return np.array(X)
 
-def get_y(folder='./data'):
-    """Format and return the ISIC labels as np arrays.
-    
-    :param folder: path to the folder where all data used by the current training task have been unarchived
-    :type folder: string
-    :return: target variable vector
-    :rtype: numpy array
-    """
-    print('Finding label files...')
-    _, y_files = _get_files(folder)
-    print('Loading labels...')
-    y = []
-    for f in y_files:
-        with open(os.path.join(folder, f)) as open_f:
-            str_y = open_f.readline().split(',')
-        y.append([float(yy) for yy in str_y])
-    return np.array(y, dtype=np.float)
+    def get_y(self, folders):
+        """Format and return the ISIC labels as np arrays.
 
+        :param folders: path to the data sample folders
+        :type folder: list of strings
+        :return: target variable vector
+        :rtype: numpy array
+        """
+        print('Finding label files...')
+        _, y_paths = _get_paths(folders)
+        print('Loading labels...')
+        y = []
+        for path in y_paths:
+            with open(path) as f:
+                str_y = f.readline().split(',')
+            y.append([float(yy) for yy in str_y])
+        return np.array(y, dtype=np.float)
 
-def save_pred(y_pred, folder='./pred'):
-    """Save prediction in PRED_FILE in folder
+    def save_pred(self, y_pred, path):
+        """Save prediction in path
 
-    :param y_pred: predicted target variable vector
-    :type y_pred: numpy array
-    :param folder: path to the folder in which to save the predicted target variable vector
-    :type folder: string
-    :return: None
-    """
-    with open(os.path.join(folder, PRED_FILE), "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(y_pred)
+        :param y_pred: predicted target variable vector
+        :type y_pred: numpy array
+        :param folder: path to the folder in which to save the predicted target variable vector
+        :type folder: string
+        :return: None
+        """
+        with open(path, "w") as f:
+            writer = csv.writer(f)
+            writer.writerows(y_pred)
 
+    def get_pred(self, path):
+        """Get predictions which were saved using the save_pred function
 
-def get_pred(folder='./pred'):
-    """Get predictions which were saved using the save_pred function
+        :param folder: path to the folder where the previously predicted target variable vector has been saved
+        :type folder:  string
+        :return: predicted target variable vector
+        :rtype: numpy array
+        """
+        with open(path, "r") as f:
+            pred_iter = csv.reader(f)
+            pred = [y for y in pred_iter]
+        return np.array(pred, copy=False, dtype=np.float)
 
-    :param folder: path to the folder where the previously predicted target variable vector has been saved
-    :type folder:  string
-    :return: predicted target variable vector
-    :rtype: numpy array
-    """
-    with open(os.path.join(folder, PRED_FILE), "r") as f:
-        pred_iter = csv.reader(f)
-        pred = [y for y in pred_iter]
-    return np.array(pred, copy=False, dtype=np.float)
+    def fake_X(self, n_samples=N_FAKE_SAMPLES):
+        """Make and return the ISIC like features data as np arrays.
 
+        :return: fake matrix of features
+        :rtype: numpy array
+        """
+        return np.random.randint(low=0, high=256, size=(n_samples, SIZE_X, SIZE_Y, SIZE_Z)).astype('uint8')
 
-def fake_X(n_samples=N_FAKE_SAMPLES):
-    """Make and return the ISIC like features data as np arrays.
+    def fake_y(self, n_samples=N_FAKE_SAMPLES):
+        """Make and return the ISIC like labels as np arrays.
 
-    :return: fake matrix of features
-    :rtype: numpy array
-    """
-    return np.random.randint(low=0, high=256, size=(n_samples, SIZE_X, SIZE_Y, SIZE_Z)).astype('uint8')
-
-
-def fake_y(n_samples=N_FAKE_SAMPLES):
-    """Make and return the ISIC like labels as np arrays.
-
-    :return: fake target variable vector
-    :rtype: numpy array
-    """
-    return np.eye(CLASSES)[np.arange(n_samples) % CLASSES].astype('uint8')
+        :return: fake target variable vector
+        :rtype: numpy array
+        """
+        return np.eye(CLASSES)[np.arange(n_samples) % CLASSES].astype('uint8')
