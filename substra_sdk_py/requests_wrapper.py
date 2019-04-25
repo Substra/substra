76ff7fb606f240afe6ef6ba632b526a80b6eb0d1
Samuel <samuel.lesuffleur@owkin.com>
2019-04-25 15:09:16 +0200

    requests wrapper: refacto: remove useless decorator

--- substra_sdk_py/requests_wrapper.py
+++ substra_sdk_py/requests_wrapper.py
@@ -1,55 +1,47 @@
-import functools
-
 import requests
 
 from . import exceptions
 from .config import requests_get_params
 
 
-def parse_response(f):
-    @functools.wraps(f)
-    def wrapper(*args, **kwargs):
-        try:
-            r = f(*args, **kwargs)
-            r.raise_for_status()
-
-        except requests.exceptions.ConnectionError as e:
-            raise exceptions.ConnectionError(e)
-
-        except requests.exceptions.Timeout as e:
-            raise exceptions.Timeout(e)
-
-        except requests.exceptions.HTTPError as e:
-            if e.response.status_code == 400:
-                raise exceptions.InvalidRequest(e)
-
-            if e.response.status_code == 404:
-                raise exceptions.NotFound(e)
-
-            if e.response.status_code == 408:
-                raise exceptions.RequestTimeout(e)
-
-            if e.response.status_code == 409:
-                raise exceptions.AlreadyExists(e)
-
-            raise exceptions.HTTPError(e)
-
-        try:
-            result = r.json()
-        except ValueError as e:
-            # we always expect JSON response from the server
-            msg = "Cannot parse response to JSON: {}".format(str(e))
-            raise exceptions.InvalidResponse(r, msg)
-
-        return result
-    return wrapper
-
-
-@parse_response
 def _req(fn, config, url, **kwargs):
+
     default_kwargs, headers = requests_get_params(config)
     kwargs.update(default_kwargs)
-    return fn(url, headers=headers, **kwargs)
+
+    try:
+        r = fn(url, headers=headers, **kwargs)
+        r.raise_for_status()
+
+    except requests.exceptions.ConnectionError as e:
+        raise exceptions.ConnectionError(e)
+
+    except requests.exceptions.Timeout as e:
+        raise exceptions.Timeout(e)
+
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 400:
+            raise exceptions.InvalidRequest(e)
+
+        if e.response.status_code == 404:
+            raise exceptions.NotFound(e)
+
+        if e.response.status_code == 408:
+            raise exceptions.RequestTimeout(e)
+
+        if e.response.status_code == 409:
+            raise exceptions.AlreadyExists(e)
+
+        raise exceptions.HTTPError(e)
+
+    try:
+        result = r.json()
+    except ValueError as e:
+        # we always expect JSON response from the server
+        msg = "Cannot parse response to JSON: {}".format(str(e))
+        raise exceptions.InvalidResponse(r, msg)
+
+    return result
 
 
 def post(config, url, data, **kwargs):
