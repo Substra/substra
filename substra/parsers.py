b52864ec7c4d314c5ece0ed552ffaedb651b3d38
Jérémy Morel <jeremy.morel@owkin.com>
2019-06-19 15:06:57 +0200

    human readable: prettier output

--- substra/parsers.py
+++ substra/parsers.py
@@ -1,6 +1,8 @@
 import json
 
 import requests
+import textwrap
+
 from substra_sdk_py.config import requests_get_params
 
 from substra.commands.api import ALGO_ASSET, OBJECTIVE_ASSET, DATA_MANAGER_ASSET, TRAINTUPLE_ASSET, \
@@ -39,9 +41,7 @@ class BaseParser:
     description_prop = 'description'
 
     list_props = ()
-    asset_props = (
-
-    )
+    asset_props = ()
 
     def __init__(self, client):
         self.client = client
@@ -57,38 +57,85 @@ class BaseParser:
         else:
             print(f'{n} {self.asset}s found.')
 
-    @handle_raw_option
-    def print_list(self, items):
-        self._print_hr_count(items)
-        for item in items:
-            print(f'* {get_prop_value(item, self.title_prop)}')
-            print(f'  Key: {get_prop_value(item, self.key_prop)}')
-            for prop in self.list_props:
-                prop_name, prop_key = prop
-                print(f'  {prop_name}: {get_prop_value(item, prop_key)}')
+    @staticmethod
+    def _print_markdown(text, indent):
+        paragraphs = text.splitlines()
+        # first paragraph
+        wrapper = textwrap.TextWrapper(subsequent_indent=' ' * indent, width=70+indent)
+        lines = wrapper.wrap(paragraphs[0])
+        for line in lines:
+            print(line)
+
+        # other paragraphs
+        wrapper.initial_indent = ' ' * indent
+        for paragraph in paragraphs[1:]:
+            if paragraph:
+                lines = wrapper.wrap(paragraph)
+                for line in lines:
+                    print(line)
+            else:
+                print()
 
     @handle_raw_option
-    def print_asset(self, item):
-        print(f'KEY: {get_prop_value(item, self.key_prop)}')
+    def print_list(self, items):
+        prop_length = self._get_list_prop_length()
+        self._print_hr_count(items)
+
+        for item in items:
+            title = get_prop_value(item, self.title_prop)
+            print()
+            print(title)
+            print('='*len(title))
+            print(f'{"KEY".ljust(prop_length)} {get_prop_value(item, self.key_prop)}')
+            for prop in self.list_props:
+                prop_name, prop_key = prop
+                print(f'{prop_name.upper().ljust(prop_length)} {get_prop_value(item, prop_key)}')
+
+    def _get_list_prop_length(self):
+        props = ['key'] + [prop for prop, _ in self.list_props]
+        max_prop_length = max(map(lambda x: len(x), props))
+        return max_prop_length
+
+    def _get_asset_prop_length(self):
+        props = ['key'] + [prop for prop, _ in self.asset_props]
+        if self.description_prop:
+            props.append('description')
+        max_prop_length = max(map(lambda x: len(x), props))
+        return max_prop_length + 1
+
+    def _print_asset_props(self, item, prop_length):
+        print(f'{"KEY".ljust(prop_length)} {get_prop_value(item, self.key_prop)}')
         for prop in self.asset_props:
             name, key = prop
             value = get_prop_value(item, key)
             if isinstance(value, list):
                 if value:
-                    print(f'{name.upper()}:')
-                    for v in value:
-                        print(f'  * {v}')
+                    print(f'{name.upper().ljust(prop_length)}', end='')
+                    padding = ' ' * prop_length
+                    for i, v in enumerate(value):
+                        if i == 0:
+                            print(f'- {v}')
+                        else:
+                            print(f'{padding}- {v}')
                 else:
-                    print(f'{name.upper()}: None')
+                    print(f'{name.upper().ljust(prop_length)} None')
             else:
-                print(f'{name.upper()}: {value}')
+                print(f'{name.upper().ljust(prop_length)} {value}')
+
+    def _print_description(self, item, prop_length):
         if self.description_prop:
             desc = get_prop_value(item, self.description_prop)
             url = desc.get('storageAddress')
             kwargs, headers = requests_get_params(self.client.config)
             r = requests.get(url, headers=headers, **kwargs)
-            print('DESCRIPTION:')
-            print(r.text)
+            print('DESCRIPTION'.ljust(prop_length), end='')
+            self._print_markdown(r.text, indent=prop_length)
+
+    @handle_raw_option
+    def print_asset(self, item):
+        prop_length = self._get_asset_prop_length()
+        self._print_asset_props(item, prop_length)
+        self._print_description(item, prop_length)
 
 
 class JsonOnlyParser:
