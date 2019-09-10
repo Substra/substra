9344ce066cf8ae277b40cccde697638a0211a987
Jérémy Morel <jeremy.morel@owkin.com>
2019-09-10 11:00:43 +0200

    tests for output format option and printer getters

--- tests/test_printers.py
+++ tests/test_printers.py
@@ -1,6 +1,7 @@
 import unittest
 
-from substra.cli.printers import find_dict_composite_key_value
+from substra.cli.printers import find_dict_composite_key_value, get_leaderboard_printer, \
+    get_asset_printer, JsonPrinter, YamlPrinter, LeaderBoardPrinter, AssetPrinter
 
 
 class TestHelperMethods(unittest.TestCase):
@@ -14,3 +15,18 @@ class TestHelperMethods(unittest.TestCase):
 
         self.assertEqual(find_dict_composite_key_value({'a': 'a'}, 'a'), 'a')
         self.assertEqual(find_dict_composite_key_value({'a': {'b': 'b'}}, 'a.b'), 'b')
+
+    def test_get_asset_printer(self):
+        self.assertIsInstance(get_asset_printer('algo', 'pretty'), AssetPrinter)
+        self.assertIsInstance(get_asset_printer('algo', 'json'), JsonPrinter)
+        self.assertIsInstance(get_asset_printer('algo', 'yaml'), YamlPrinter)
+
+        self.assertIsInstance(get_asset_printer('foo', 'pretty'), JsonPrinter)
+        self.assertIsInstance(get_asset_printer('foo', 'json'), JsonPrinter)
+        self.assertIsInstance(get_asset_printer('foo', 'yaml'), YamlPrinter)
+
+    def test_get_leaderboard_printer(self):
+        self.assertIsInstance(get_leaderboard_printer('json'), JsonPrinter)
+        self.assertIsInstance(get_leaderboard_printer('yaml'), YamlPrinter)
+        self.assertIsInstance(get_leaderboard_printer('pretty'), LeaderBoardPrinter)
+        self.assertIsInstance(get_leaderboard_printer('foo'), JsonPrinter)
