e5f610d597186e7caeef2bb9c53dab88bb83f81c
Maël Debon <mael.debon@owkin.com>
2019-12-13 14:57:51 +0100

    Fix tests & flake8

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -208,7 +208,8 @@ def test_command_add_composite_traintuple(mocker, workdir, params, message, exit
 
 def test_command_add_testtuple_no_data_samples(mocker, workdir):
     m = mock_client_call(mocker, 'add_testtuple', response={})
-    client_execute(workdir, ['add', 'testtuple', '--objective-key', 'foo', '--traintuple-key', 'foo'])
+    client_execute(workdir, ['add', 'testtuple', '--objective-key', 'foo',
+                             '--traintuple-key', 'foo'])
     assert m.is_called()
 
 
@@ -294,7 +295,7 @@ def test_command_download(workdir, mocker):
 
 
 def test_command_cancel_compute_plan(workdir, mocker):
-    m = mock_client_call(mocker, 'cancel_compute_plan')
+    m = mock_client_call(mocker, 'cancel_compute_plan', datastore.COMPUTE_PLAN)
     client_execute(workdir, ['cancel', 'compute_plan', 'fakekey'])
     assert m.is_called()
 
