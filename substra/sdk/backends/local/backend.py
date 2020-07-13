074d07e61612d4ad630cda8bc7bbe446c0c34295
thaisdeboisfosse <thais.de-boisfosse@owkin.com>
2020-07-13 17:05:35 +0200

    Fix add compute plan circular deps

--- substra/sdk/backends/local/backend.py
+++ substra/sdk/backends/local/backend.py
@@ -720,7 +720,12 @@ class Local(base.BaseBackend):
     def cancel_compute_plan(self, compute_plan_id):
         raise NotImplementedError
 
-    def _add_compute_plan(self, spec: schemas.ComputePlanSpec, exist_ok: bool, spec_options: dict = None):
+    def _add_compute_plan(
+        self,
+        spec: schemas.ComputePlanSpec,
+        exist_ok: bool,
+        spec_options: dict = None
+    ):
 
         # validation
         traintuples = dict()
@@ -751,13 +756,19 @@ class Local(base.BaseBackend):
         compute_plan = self._db.add(compute_plan, exist_ok)
 
         # go through the traintuples sorted by rank
-        for id_, rank in {id_: rank for id_, rank in sorted(visited.items(), key=lambda item: item[1])}.items():
+        for id_, rank in {
+            id_: rank
+            for id_, rank
+            in sorted(visited.items(), key=lambda item: item[1])
+        }.items():
             traintuple = traintuples[id_]
             traintuple_spec = schemas.TraintupleSpec(
                 algo_key=traintuple.algo_key,
                 data_manager_key=traintuple.data_manager_key,
                 train_data_sample_keys=traintuple.train_data_sample_keys,
-                in_models_keys=[compute_plan.id_to_key[parent_id] for parent_id in traintuple.in_models_ids],
+                in_models_keys=[
+                    compute_plan.id_to_key[parent_id] for parent_id in traintuple.in_models_ids
+                ],
                 tag=traintuple.tag,
                 compute_plan_id=compute_plan.compute_plan_id,
                 rank=rank,
@@ -765,22 +776,23 @@ class Local(base.BaseBackend):
             )
             traintuple = self.add(traintuple_spec, exist_ok, spec_options)
             # Get the compute plan as it has been modified when adding the traintuple
-            # compute_plan = self._db.get(schemas.Type.ComputePlan, key=compute_plan.compute_plan_id)
             compute_plan.id_to_key[id_] = traintuple["key"]
-            # self._db.update(compute_plan)
 
         return compute_plan
 
     def __get_rank(self, node, visited, node_deps, tuples):
         if node in node_deps:
-            raise Exception("Cycle !")
+            raise exceptions.InvalidRequest("missing dependency among inModels IDs", 400)
         if node in visited:
             return visited[node]
+        node_deps.add(node)
 
         if len(tuples[node].in_models_ids) == 0:
             rank = 0
         else:
-            rank = 1 + max([self.__get_rank(x, visited, set(), tuples) for x in tuples[node].in_models_ids])
+            rank = 1 + max([
+                self.__get_rank(x, visited, node_deps, tuples)
+                for x in tuples[node].in_models_ids
+            ])
         visited[node] = rank
-        node_deps.add(node)
         return rank
