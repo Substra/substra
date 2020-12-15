b38b48790f294173e24599d2bba8fb077c6ad6b9
thaisdeboisfosse <thais.de-boisfosse@owkin.com>
2020-12-15 19:21:02 +0100

    remarks

--- tests/sdk/test_debug.py
+++ tests/sdk/test_debug.py
@@ -23,15 +23,13 @@ def test_client_multi_nodes_cp(asset_factory):
     """Assert that there is one CP local folder per node"""
     client = substra.Client(debug=True)
 
-    dataset_query = asset_factory.create_dataset()
-    dataset_query.metadata = {substra.DEBUG_OWNER: 'owner_1'}
-    dataset_key = client.add_dataset(dataset_query)
+    dataset_query = asset_factory.create_dataset(metadata={substra.DEBUG_OWNER: 'owner_1'})
+    dataset_1_key = client.add_dataset(dataset_query)
 
-    dataset_2_query = asset_factory.create_dataset()
-    dataset_2_query.metadata = {substra.DEBUG_OWNER: 'owner_2'}
+    dataset_2_query = asset_factory.create_dataset(metadata={substra.DEBUG_OWNER: 'owner_2'})
     dataset_2_key = client.add_dataset(dataset_2_query)
 
-    data_sample = asset_factory.create_data_sample(datasets=[dataset_key], test_only=False)
+    data_sample = asset_factory.create_data_sample(datasets=[dataset_1_key], test_only=False)
     sample_1_key = client.add_data_sample(data_sample)
 
     data_sample = asset_factory.create_data_sample(datasets=[dataset_2_key], test_only=False)
@@ -44,7 +42,7 @@ def test_client_multi_nodes_cp(asset_factory):
     cp.traintuples = [
         substra.sdk.schemas.ComputePlanTraintupleSpec(
             algo_key=algo_key,
-            data_manager_key=dataset_key,
+            data_manager_key=dataset_1_key,
             traintuple_id=1,
             train_data_sample_keys=[sample_1_key],
         ),
