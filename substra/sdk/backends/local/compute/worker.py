54d0df5961bfdc71d9246b21b23456437fc03078
Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
2021-04-14 10:01:47 +0200

    debug mode: move the chainkeys to local-worker (#257)

--- substra/sdk/backends/local/compute/worker.py
+++ substra/sdk/backends/local/compute/worker.py
@@ -22,6 +22,8 @@ from substra.sdk import schemas, fs, models
 from substra.sdk.backends.local import dal
 from substra.sdk.backends.local.compute import spawner
 
+LOCAL_DIR = pathlib.Path.cwd() / "local-worker"
+
 _CONTAINER_MODEL_PATH = "/sandbox/model"
 
 _VOLUME_INPUT_DATASAMPLES = {"bind": "/sandbox/data", "mode": "ro"}
@@ -54,7 +56,7 @@ class Worker:
     """ML Worker."""
 
     def __init__(self, db: dal.DataAccess, support_chainkeys: bool, chainkey_dir=None):
-        self._wdir = os.path.join(os.getcwd(), "local-worker")
+        self._wdir = LOCAL_DIR
         self._db = db
         self._spawner = spawner.get()
         self._support_chainkeys = support_chainkeys
