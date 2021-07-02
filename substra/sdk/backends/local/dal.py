ead32bcb9a32b0ad7175cb27f24d0528e7b33c41
Kelvin Moutet <kelvin.moutet@owkin.com>
2021-07-02 16:36:56 +0200

    fix: fix local download

--- substra/sdk/backends/local/dal.py
+++ substra/sdk/backends/local/dal.py
@@ -55,7 +55,7 @@ class DataAccess:
             return False
 
     def _get_asset_content_filename(self, type_):
-        if type_ == schemas.Type.Algo:
+        if type_ in [schemas.Type.Algo, schemas.Type.CompositeAlgo, schemas.Type.AggregateAlgo]:
             asset_name = "algo.tar.gz"
             field_name = "algorithm"
 
