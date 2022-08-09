0e1901f92d339453a1bd90b9558140fc5f046e39
Thaïs de Boisfossé <4139100+Esadruhn@users.noreply.github.com>
2022-08-09 14:12:37 +0200

    chore: add license (#252)

--- setup.py
+++ setup.py
@@ -1,17 +1,3 @@
-# Copyright 2018 Owkin, inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Packaging settings."""
 import os
 from codecs import open
@@ -37,9 +23,8 @@ setup(
     description="Substra CLI for interacting with substra-backend",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/SubstraFoundation/substra",
-    author="Owkin",
-    author_email="fldev@owkin.com",
+    url="https://connect-docs.owkin.com/",
+    author="Owkin, Inc.",
     license="Apache 2.0",
     classifiers=[
         "Intended Audience :: Developers",
@@ -47,6 +32,7 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords=["cli", "substra"],
     packages=find_packages(exclude=["tests*"]),
--- tests/datastore.py
+++ tests/datastore.py
@@ -1,18 +1,3 @@
-# Copyright 2018 Owkin, inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
 METRIC = {
     "key": "d5002e1c-d50b-d5de-5341-df8a7b7d11b6",
     "name": "Skin Lesion Classification Challenge",
