8bc3be56db5ef4a38044f936cbff675372a6f8e0
thaisdeboisfosse <thais.de-boisfosse@owkin.com>
2022-10-04 09:48:15 +0200

    manage case empty graph - cp rank calculation
    Signed-off-by: thaisdeboisfosse <thais.de-boisfosse@owkin.com>

--- substra/sdk/graph.py
+++ substra/sdk/graph.py
@@ -17,44 +17,9 @@ def _get_inverted_node_graph(node_graph, node_to_ignore):
     return inverted
 
 
-def compute_ranks(
-    node_graph: typing.Dict[str, typing.List[str]],
-    node_to_ignore: typing.Set[str] = None,
-    ranks: typing.Dict[str, int] = None,
-) -> typing.Dict[str, int]:
-    """Compute the ranks of the nodes in the graph.
-
-    Args:
-        node_graph (typing.Dict[str, typing.List[str]]):
-            Dict {node_id: list of nodes it depends on}.
-            Node graph keys must not contain any node to ignore.
-        node_to_ignore (typing.Set[str], optional): List of nodes to ignore.
-            Defaults to None.
-        ranks (typing.Dict[str, int]): Already computed ranks. Defaults to None.
-
-    Raises:
-        exceptions.InvalidRequest: If the node graph contains a cycle
-
-    Returns:
-        typing.Dict[str, int]: Dict { node_id : rank }
-    """
-    ranks = ranks or dict()
-    visited = set()
-    node_to_ignore = node_to_ignore or set()
-
-    extra_nodes = set(node_graph.keys()).intersection(node_to_ignore)
-    if len(extra_nodes) > 0:
-        raise ValueError(f"node_graph keys should not contain any node to ignore: {extra_nodes}")
-
-    inverted_node_graph = _get_inverted_node_graph(node_graph, node_to_ignore)
-
-    # Assign rank 0 to nodes without deps
-    for node, dependencies in node_graph.items():
-        if node not in node_to_ignore:
-            actual_deps = [dep for dep in dependencies if dep not in node_to_ignore]
-            if len(actual_deps) == 0:
-                ranks[node] = 0
-
+def _breadth_first_tranversal_rank(
+    ranks: typing.Dict[str, int], inverted_node_graph: typing.Dict[str, typing.List[str]]
+):
     edges = set()
     queue = [node for node in ranks]
     visited = set(queue)
@@ -85,5 +50,49 @@ def compute_ranks(
                 )
             else:
                 edges.add(edge)
+    return ranks
+
+
+def compute_ranks(
+    node_graph: typing.Dict[str, typing.List[str]],
+    node_to_ignore: typing.Set[str] = None,
+    ranks: typing.Dict[str, int] = None,
+) -> typing.Dict[str, int]:
+    """Compute the ranks of the nodes in the graph.
+
+    Args:
+        node_graph (typing.Dict[str, typing.List[str]]):
+            Dict {node_id: list of nodes it depends on}.
+            Node graph keys must not contain any node to ignore.
+        node_to_ignore (typing.Set[str], optional): List of nodes to ignore.
+            Defaults to None.
+        ranks (typing.Dict[str, int]): Already computed ranks. Defaults to None.
+
+    Raises:
+        exceptions.InvalidRequest: If the node graph contains a cycle
+
+    Returns:
+        typing.Dict[str, int]: Dict { node_id : rank }
+    """
+    ranks = ranks or dict()
+    node_to_ignore = node_to_ignore or set()
+
+    if len(node_graph) == 0:
+        return dict()
+
+    extra_nodes = set(node_graph.keys()).intersection(node_to_ignore)
+    if len(extra_nodes) > 0:
+        raise ValueError(f"node_graph keys should not contain any node to ignore: {extra_nodes}")
+
+    inverted_node_graph = _get_inverted_node_graph(node_graph, node_to_ignore)
+
+    # Assign rank 0 to nodes without deps
+    for node, dependencies in node_graph.items():
+        if node not in node_to_ignore:
+            actual_deps = [dep for dep in dependencies if dep not in node_to_ignore]
+            if len(actual_deps) == 0:
+                ranks[node] = 0
+
+    ranks = _breadth_first_tranversal_rank(ranks=ranks, inverted_node_graph=inverted_node_graph)
 
     return ranks
