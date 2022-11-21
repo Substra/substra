b0eb035188d1307284646f8095983652e4bf84f1
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2022-11-21 11:33:13 +0100

    doc url
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description="Substra CLI for interacting with substra-backend",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://connect-docs.owkin.com/",
+    url="https://docs.substra.org/en/latest/",
     author="Owkin, Inc.",
     license="Apache 2.0",
     classifiers=[
