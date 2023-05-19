6764a16b59e9185199e6381591659af730c51ea9
Guilhem Barthes <guilhem.barthes@owkin.com>
2023-05-19 17:42:39 +0200

    fix: `TASK_LIST`
    Signed-off-by: Guilhem Barthes <guilhem.barthes@owkin.com>

--- tests/datastore.py
+++ tests/datastore.py
@@ -559,10 +559,8 @@ TESTTASK = {
     },
 }
 
-TASK_LIST = [
-    {key: value for key, value in task.items() if key not in {"inputs", "outputs"}}
-    for task in [PREDICTTASK, AGGREGATETASK, COMPOSITE_TRAINTASK, TRAINTASK, TESTTASK]
-]
+TASK_LIST = [PREDICTTASK, AGGREGATETASK, COMPOSITE_TRAINTASK, TRAINTASK, TESTTASK]
+
 
 COMPUTE_PLAN = {
     "key": "e983a185-5368-bd0a-0190-183af9a8e560",
