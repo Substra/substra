e356eb9f3ef530ebfe5d69f12a01f43ca09e5950
ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>
2023-08-31 17:18:30 +0200

    temp
    Signed-off-by: ThibaultFy <50656860+ThibaultFy@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "urllib3<2",
         "docker",
         "pyyaml",
-        "pydantic>=2.3.0, <3.0.0",
+        "pydantic<3.0.0",
         "tqdm",
         "python-slugify",
     ],
